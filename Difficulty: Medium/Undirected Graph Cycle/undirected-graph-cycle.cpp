059d4032c84ccfabb0d5c4874ff9dec3f093fc6e
Satvik Jain <satvik.jain.kht@gmail.com>
2024-12-07 22:25:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -4,40 +4,39 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
+  private:
+    bool dfs(int n, vector<int>& vis, vector<vector<int>> &adj, int ele, int adjacent){
+        vis[ele] = 1;
+        for (int j: adj[ele]){
+            if(!vis[j]){
+                if (dfs(n, vis, adj, j, ele)) return true;
+            }
+            else if(vis[j] == 1 && j!=adjacent){
+                return true;
+            }
+            
+        }
+        return false;
+    }
   public:
     // Function to detect cycle in an undirected graph.
     bool isCycle(vector<vector<int>>& adj) {
         // Code here
         int n = adj.size();
-        vector<int> vis(n, 0);
-        for (int j = 0; j < adj.size(); j++){
-            if (!vis[j]){
-                queue <pair<int,int>> q;
-                q.push({j,-1});
-                vis[j]++;
-                while(!q.empty()){
-                    int AdjEle = q.front().second;
-                    int temp = q.front().first;
-                    q.pop();
-                    for(auto i : adj[temp]){
-                        if(vis[i]!=0 && i!=AdjEle){
-                            return true;
-                        }
-                        if(!vis[i]){
-                            vis[i]=1;
-                            q.push({i, temp});
-                        }
-                        
-                    }
-                    
-                }
-                
+        vector<int> vis(n , 0);
+        for (int i =0; i < n ; i++){
+            if (!vis[i]){
+                int adjacent = -1;
+                int ele = i;
+                if(dfs(n, vis, adj, ele ,adjacent)) return true;
             }
         }
         return false;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {

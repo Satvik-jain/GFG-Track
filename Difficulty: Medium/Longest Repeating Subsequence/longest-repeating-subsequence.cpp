abc5167b9fd6c6e6fcc50bb7a5fd7bc58f1ab2af
Satvik Jain <satvik.jain.kht@gmail.com>
2025-04-14 22:29:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Repeating Subsequence/longest-repeating-subsequence.cpp
+++ Difficulty: Medium/Longest Repeating Subsequence/longest-repeating-subsequence.cpp
@@ -22,6 +22,12 @@ class Solution {
                 }
             }
         }
+        // for(int i = 0; i < n+1; i++){
+        //     for(int j = 0; j < m+1; j++){
+        //         cout << t[i][j];
+        //     }
+        //     cout << endl;
+        // }
         return t[n][m];
     }
 };

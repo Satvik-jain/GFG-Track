69767b1cecc2e4162fa17ef6ad635cfa3fbc8175
Satvik Jain <satvik.jain.kht@gmail.com>
2025-05-31 13:01:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,67 +1,39 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-  private:
-    bool dfs(int n, vector<int>& vis, vector<vector<int>> &adj, int ele, int adjacent){
-        vis[ele] = 1;
-        for (int j: adj[ele]){
+  public:
+    bool f(vector<int> v[], vector<int> &vis){
+        for(int j = 0; j < vis.size(); j++){
             if(!vis[j]){
-                if (dfs(n, vis, adj, j, ele)) return true;
-            }
-            else if(vis[j] == 1 && j!=adjacent){
-                return true;
+                queue<pair<int, int>> q;
+                q.push({j, -1});
+                vis[j] = 1;
+                while(!q.empty()){
+                    int temp = q.front().first;
+                    // cout << temp << endl;
+                    int old = q.front().second;
+                    q.pop();
+                    for (int i : v[temp]){
+                        if((i != old) && (vis[i] == 1)) return true;
+                        if (!vis[i]){
+                            vis[i] = 1;
+                            q.push({i, temp});
+                        }
+                    }
+                } 
             }
             
         }
         return false;
+        
     }
-  public:
-    // Function to detect cycle in an undirected graph.
-    bool isCycle(vector<vector<int>>& adj) {
+    bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
-        int n = adj.size();
-        vector<int> vis(n , 0);
-        for (int i =0; i < n ; i++){
-            if (!vis[i]){
-                int adjacent = -1;
-                int ele = i;
-                if(dfs(n, vis, adj, ele ,adjacent)) return true;
-            }
+        int E = edges.size();
+        vector<int> v[V];
+        for (int i = 0; i < E; i++){
+            v[edges[i][1]].push_back(edges[i][0]);
+            v[edges[i][0]].push_back(edges[i][1]);
         }
-        return false;
+        vector<int> vis(V, 0);
+        return f(v, vis);
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> adj(V);
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        Solution obj;
-        bool ans = obj.isCycle(adj);
-        if (ans)
-            cout << "1\n";
-        else
-            cout << "0\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends

43309ec510483db003edabf8afef2f344b6b9ede
Satvik Jain <satvik.jain.kht@gmail.com>
2025-06-09 21:11:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
+++ Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
@@ -4,53 +4,39 @@ class Solution {
   public:
     int countDistinctIslands(vector<vector<int>>& grid) {
         // code here
-        int n = grid.size(); int m = grid[0].size();
-        vector<pair<int, int>> v;
-        set <vector<pair<int,int>>> s;
-        queue <pair<int, int>> q;
-        vector<vector<int>> vis(n, vector<int>(m , 0));
+        int m = grid[0].size();
+        int n = grid.size();
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        set<vector<pair<int,int>>> s;
         for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                v.clear();
-                if (vis[i][j] == 0 && grid[i][j] == 1){
-                    q.push({i,j});
+            for(int j = 0; j < m; j++){
+                vector<pair<int, int>> v;
+                if (grid[i][j] == 1 && vis[i][j] == 0){
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    // vis[i][j] = 1;
                     while(!q.empty()){
-                        int newi = q.front().first;
-                        int newj = q.front().second;
+                        int temp_i = q.front().first;
+                        int temp_j = q.front().second;
                         q.pop();
-                        for(int a: {-1, 0, 1}){
-                            for (int b: {-1, 0, 1})
-                            {
-                                int r = newi+a;
-                                int c = newj+b;
+                        for (int a : {-1, 0, 1}){
+                            for (int b : {-1, 0, 1}){
                                 if (abs(a) + abs(b) == 2) continue;
-                                if (r<0 || c<0 || r>n-1 || c > m-1){
-                                    continue;
-                                }
-                                else{
-                                    if(grid[r][c] == 1 && vis[r][c] == 0){
-                                        vis[r][c] = 1;
-                                        q.push({r, c});
-                                        v.push_back({r-i, c-j});
-                                    }
+                                int r = temp_i + a;
+                                int c = temp_j + b;
+                                if (r < 0 || c < 0 || r > n-1 || c > m-1) continue;
+                                if (grid[r][c] == 1 && vis[r][c] == 0){
+                                    q.push({r, c});
+                                    vis[r][c] = 1;
+                                    v.push_back({r-i, c-j});
                                 }
                             }
                         }
                     }
                 }
-                if(v.size()) s.insert(v);
+                if (v.size()) s.insert(v);
             }
         }
-        
-//         cout << "Distinct Islands:\n";
-// for (const auto& shape : s) {
-//     cout << "[ ";
-//     for (const auto& cell : shape) {
-//         cout << "(" << cell.first << "," << cell.second << ") ";
-//     }
-//     cout << "]\n";
-// }
-
         return s.size();
     }
 };

5127a14f4574d8ecf768df589c0d425eeffe9ca2
Satvik Jain <satvik.jain.kht@gmail.com>
2025-06-13 21:39:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
@@ -1,63 +1,41 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-  private:
-    bool dfs(vector<int>& vis, vector<vector<int>> & adj, int i){
-        for (j: adj[i]){
-            if (vis[j] == -1){
-                vis[j] = !vis[i];
-                dfs(vis, adj, j);
-            }
-            else if(vis[i] == vis[j]){
-                return false;
-            }
-        }
-        return true;
-    }
   public:
-    bool isBipartite(vector<vector<int>>& adj) {
+    bool isBipartite(int V, vector<vector<int>> &edges) {
         // Code here
-        vector <int> vis(adj.size(), -1);
-        for (int  i =0 ; i < adj.size(); i++){
-            if(vis[i]==-1){
-                vis[i] = 0;
-                if (!dfs(vis, adj, i)) return false;
+        int n = edges.size();
+        int m = edges[0].size();
+        queue <int> q;
+        vector<int> v(n, -1);
+        
+        int n2 = 0;
+        for (auto& edge : edges) {
+            n2 = max(n2, max(edge[0], edge[1]));
+        }
+
+        vector<vector<int>> adj(n2 + 1);
+    
+        for (auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u); 
+        }
+        
+        q.push(0);
+        v[0] = 0;
+        
+        while(!q.empty()){
+            int temp = q.front();
+            q.pop();
+            for (int i: adj[temp]){
+                if (v[i]==-1){
+                    v[i] = !v[temp];
+                    q.push(i);
+                }
+                else{
+                    if (v[i] == v[temp]) return false;
+                }
             }
         }
         return true;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> adj(V);
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        Solution obj;
-        bool ans = obj.isBipartite(adj);
-        if (ans)
-            cout << "true\n";
-        else
-            cout << "false\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends

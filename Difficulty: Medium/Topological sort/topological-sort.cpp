04f250bf85ab2093450c4dc25f36f072e1e0ad79
Satvik Jain <satvik.jain.kht@gmail.com>
2025-06-16 20:00:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,36 +1,31 @@
 class Solution {
   public:
-    void dfs(int i, vector<int> grid[], vector<int>& v, vector<int> & ans, stack<int>& s){
-        v[i] = 1;
-        for (auto j : grid[i]){
-            if (v[j] == -1){
-                dfs(j, grid, v, ans, s);
+    void dfs(int node, vector<int> grid[], vector<bool>& visited, vector<int>& ans) {
+        visited[node] = true;
+        for (int neighbor : grid[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, grid, visited, ans);
             }
         }
-        s.push(i);
+        ans.push_back(node); // push after exploring all neighbors
     }
+
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        // code here
         vector<int> grid[V];
-        for (int i = 0; i < edges.size(); i++){
-            grid[edges[i][0]].push_back(edges[i][1]);
+        for (auto &e : edges) {
+            grid[e[0]].push_back(e[1]);
         }
-        
-        vector<int> v(V, -1);
+
+        vector<bool> visited(V, false);
         vector<int> ans;
-        
-        stack<int> s;
-        
-        for(int i = 0; i < V; i++){
-            if (v[i] == -1){
-                dfs(i, grid, v, ans, s);
+
+        for (int i = 0; i < V; ++i) {
+            if (!visited[i]) {
+                dfs(i, grid, visited, ans);
             }
         }
-        // s to ans
-        for (auto i: s){
-            ans.push_back(i);
-            s.pop();
-        }
+
+        reverse(ans.begin(), ans.end()); // reverse post-order DFS result
         return ans;
     }
 };
